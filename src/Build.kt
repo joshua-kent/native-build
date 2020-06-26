a714e074edb7d44b7b879e5470e6056f356e1700
Joshua Kent <hyperxbeat@live.com>
2020-06-26 23:19:05 +0100

    Tidied source code

---
+++ src/Build.kt
@@ -0,0 +1,89 @@
+package com.nativebuild.build
+
+import java.io.File
+
+object build {
+
+    // TODO: Add documentation for functions and build object
+
+    // path of main directory
+    val nativeDirString = "C:\\kotlin-native"
+    // used as a template for installation place
+    val nativeDestDirVersionlessString = "${nativeDirString}\\kotlin-native-windows-"
+    // same as above, but able to be used as regex string with Powershell
+    val nDDVS_ps1 = "C:\\\\kotlin-native\\\\kotlin-native-windows-" //
+    // where kotlin native will be installed
+    val nativeDestDirString = "${nativeDestDirVersionlessString}${KotlinVersion.CURRENT}"
+    // used to check if the current installation is installed
+    val nativeDestDir = File(nativeDestDirString)
+    // the .jar file to be included to be used by path
+    val jarFile = "${nativeDirString}\\native-build.jar"
+    // URL to download from
+    val targetURL = "https://github.com/JetBrains/kotlin/releases/download/v${KotlinVersion.CURRENT}/kotlin-native-windows-${KotlinVersion.CURRENT}.zip"
+    // current path of .jar file
+    var jarPath = object {}.javaClass.protectionDomain.codeSource.location.toURI().path
+    // path of .bat file in path
+    val batFileString = "${nativeDirString}\\native-build.bat"
+    // as java file to be manipulated
+    val batFile = File(batFileString)
+    // current java runtime, used to call start processes
+    val javaRuntime : Runtime = Runtime.getRuntime()
+
+    init {
+        if (jarPath.startsWith('/')) jarPath = jarPath.drop(1)
+        jarPath = jarPath.replace('/', '\\')
+    }
+
+    /**
+     * Runs a PowerShell command from runtime
+     *
+     * @param comment What to output while running command
+     * @param command What to run (as PowerShell)
+     */
+    private fun runTemplate(comment: String, command: String) {
+        // TODO: add progress bar functionality
+        println(comment)
+        val commandArr = arrayOf("powershell.exe", "-Command", "\"" + command + "\"")
+        val proc = this.javaRuntime.exec(commandArr)
+        proc.waitFor()
+    }
+
+    /**Downloads .zip file from source.*/
+    fun downloadZip() = runTemplate("Downloading from $targetURL...",
+        "Invoke-WebRequest \"$targetURL\" -OutFile $nativeDestDirString.zip")
+
+    fun removeOldInstallation() = runTemplate("Deleting previous installation...",
+                "Remove-Item $nativeDestDirString -Recurse")
+
+    fun extractZip() = runTemplate("Extracting files from .zip file...",
+        "Expand-Archive -LiteralPath '$nativeDestDirString.zip' -DestinationPath $nativeDirString -Force")
+
+    fun deleteZip() = runTemplate("Deleting temporary .zip file...",
+        "Remove-Item $nativeDestDirString.zip")
+
+    fun addJarToPath() = runTemplate("Adding native-build.jar to $nativeDirString...",
+        "Copy-Item '$jarPath' -Destination $jarFile -Force")
+
+    fun appendToBatFile() {
+        val newBatFile = batFile.createNewFile()
+
+        if (newBatFile) {
+            println("Creating executable native-build.bat file")
+            batFile.appendText("@echo off\njava -jar $jarFile")
+        }
+    }
+
+    /**
+     * Removes pre-existing kotlin-native variables from path.
+     * Thanks to Chris Dent's answer here:
+     * https://stackoverflow.com/questions/39010405/powershell-how-to-delete-a-path-in-the-path-environment-variable
+     */
+    fun removeOldVersionPaths() = runTemplate("Removing outdated paths if they exists...",
+        "[Environment]::SetEnvironmentVariable('Path', (([Environment]::GetEnvironmentVariable('Path', 'User').Split(';') -NotMatch '$nDDVS_ps1') -Join ';'), 'User')")
+
+    fun addNewVersionToPath() = runTemplate("Adding new Kotlin/Native version to path...",
+        "[Environment]::SetEnvironmentVariable('Path', [Environment]::GetEnvironmentVariable('Path', 'User') + ';$nativeDestDirString\\bin', 'User')")
+
+    fun addBaseToPath() = runTemplate("Adding C:\\kotlin-native to path if it isn't already added...",
+        "${'$'}path = [Environment]::GetEnvironmentVariable('Path', 'User'); if (!(${'$'}path.Split(';') | Where-Object {${'$'}_ -eq 'C:\\kotlin-native'})) {[Environment]::SetEnvironmentVariable('Path', ${'$'}path + ';C:\\kotlin-native', 'User')}")
+}
--- src/NativeBuild.kt
+++ src/NativeBuild.kt
@@ -1,42 +1,16 @@
-import java.io.File
-import java.lang.Process
-import java.lang.Runtime
+import com.nativebuild.build.build
 
-fun main(args : Array<String>) {
+fun main(args: Array<String>) {
     val TESTING = false // enable to stop building when testing new features
-    val VERSION = "1.0.0rc2" // TODO: for future update, to check for new major releases on GitHub
+    val VERSION = "1.0.0rc3" // TODO: for future update, to check for new major releases on GitHub
 
     var run = false
     var destExists = false
 
-    // path of main directory
-    val nativeDirString = "C:\\kotlin-native"
-    // used as a template for installation place
-    val nativeDestDirVersionlessString = "${nativeDirString}\\kotlin-native-windows-"
-    // same as above, but able to be used as regex string with Powershell
-    val nDDVS_ps1 = "C:\\\\kotlin-native\\\\kotlin-native-windows-" //
-    // where kotlin native will be installed
-    val nativeDestDirString = "${nativeDestDirVersionlessString}${KotlinVersion.CURRENT}"
-    // used to check if the current installation is installed
-    val nativeDestDir = File(nativeDestDirString)
-    // the .jar file to be included to be used by path
-    val jarFile = "${nativeDirString}\\native-build.jar"
-    // URL to download from
-    val targetURL = "https://github.com/JetBrains/kotlin/releases/download/v${KotlinVersion.CURRENT}/kotlin-native-windows-${KotlinVersion.CURRENT}.zip"
-    // current path of .jar file
-    var jarPath = object {}.javaClass.protectionDomain.codeSource.location.toURI().path
-    if (jarPath.startsWith('/')) jarPath = jarPath.drop(1)
-    jarPath = jarPath.replace('/', '\\')
-    // path of .bat file in path
-    val batFileString = "${nativeDirString}\\native-build.bat"
-    // as java file to be manipulated
-    val batFile = File(batFileString)
-
-
     println("Running Kotlin/Native Builder v$VERSION")
     println("Current Kotlin version: ${KotlinVersion.CURRENT}\n")
 
-    if (!nativeDestDir.exists()) {
+    if (!build.nativeDestDir.exists()) {
         println("A new version of Kotlin/Native should be available!")
         println("Do you want to install? (Y/N)")
         val answer : String = readLine()!!
@@ -53,89 +27,32 @@ fun main(args : Array<String>) {
         if (!TESTING) {
             println("\n--- These steps usually take a while, please wait. ---\n")
 
-            // TODO: add progress bar
-
-            // downloads .zip file from source
-            println("Downloading from ${targetURL}...")
-            val p1_c = arrayOf(
-                "powershell.exe", "-Command",
-                "\"Invoke-WebRequest \"${targetURL}\" -OutFile ${nativeDestDirString}.zip\""
-            )
-            val p1: Process = Runtime.getRuntime().exec(p1_c)
-            p1.waitFor()
+            build.downloadZip()
 
             // deleting previous installation if exists
-            if (destExists) {
-                println("Deleting previous installation...")
-                val p2_c = arrayOf("powershell.exe", "-Command", "\"Remove-Item ${nativeDestDirString}\" -Recurse")
-                val p2: Process = Runtime.getRuntime().exec(p2_c)
-                p2.waitFor()
-            }
+            if (destExists) build.removeOldInstallation()
 
             // extracts .zip file to folder
-            println("Extracting files from .zip file...")
-            val p3_c = arrayOf(
-                "powershell.exe", "-Command",
-                "\"Expand-Archive -LiteralPath '${nativeDestDirString}.zip' -DestinationPath ${nativeDirString}\" -Force"
-            )
-            val p3: Process = Runtime.getRuntime().exec(p3_c)
-            p3.waitFor()
+            build.extractZip()
 
             // deletes .zip file
-            println("Deleting temporary .zip file...")
-            val p4_c = arrayOf("powershell.exe", "-Command", "\"Remove-Item ${nativeDestDirString}.zip\"")
-            val p4: Process = Runtime.getRuntime().exec(p4_c)
-            p4.waitFor()
+            build.deleteZip()
 
             // adding .jar file to C:\kotlin-native
-            if (jarPath != jarFile) {
-                println("Adding kotlin-native-build.jar to ${nativeDirString}...")
-                val p5_c = arrayOf(
-                    "powershell.exe", "-Command",
-                    "\"Copy-Item '${jarPath}' -Destination $jarFile -Force\""
-                )
-                val p5: Process = Runtime.getRuntime().exec(p5_c)
-                p5.waitFor()
-            }
+            if (build.jarPath != build.jarFile) build.addJarToPath()
 
             // creates bat file if it doesn't exist and returns true
-            val newBatFile = batFile.createNewFile()
-
-            if (newBatFile) {
-                println("Creating executable native-build.bat file")
-                batFile.appendText("@echo off\njava -jar $jarFile")
-            }
+            build.appendToBatFile()
 
             /* adding to path */
 
-            println("Removing outdated paths if they exist...")
-            /* removes pre-existing kotlin-native variables from path
-            Thanks to Chris Dent's answer here:
-            https://stackoverflow.com/questions/39010405/powershell-how-to-delete-a-path-in-the-path-environment-variable */
-            val p6_c = arrayOf(
-                "powershell.exe", "-Command",
-                "\"[Environment]::SetEnvironmentVariable('Path', (([Environment]::GetEnvironmentVariable('Path', 'User').Split(';') -NotMatch '${nDDVS_ps1}') -Join ';'), 'User')\""
-            )
-            val p6: Process = Runtime.getRuntime().exec(p6_c)
-            p6.waitFor()
+            build.removeOldVersionPaths()
 
             // adds new kotlin-native variable to path
-            println("Adding new Kotlin/Native version to path...")
-            val p7_c = arrayOf(
-                "powershell.exe", "-Command",
-                "\"[Environment]::SetEnvironmentVariable('Path', [Environment]::GetEnvironmentVariable('Path', 'User')  + ';${nativeDestDirString}\\bin', 'User')\""
-            )
-            val p7: Process = Runtime.getRuntime().exec(p7_c)
-            p7.waitFor()
+            build.addNewVersionToPath()
 
             // adding C:\kotlin-native to path
-            println("Adding C:\\kotlin-native to path if it isn't already added...")
-            val p8_c = arrayOf(
-                "powershell.exe", "-Command",
-                "\"${'$'}path = [Environment]::GetEnvironmentVariable('Path', 'User'); if (!(${'$'}path.Split(';') | Where-Object {${'$'}_ -eq 'C:\\kotlin-native'})) {[Environment]::SetEnvironmentVariable('Path', ${'$'}path + ';C:\\kotlin-native', 'User')}\""
-            )
-            val p8: Process = Runtime.getRuntime().exec(p8_c)
-            p8.waitFor()
+            build.addBaseToPath()
 
             // ending statements
             println("\nIf a previous installation exists, it will not have been uninstalled, but it may have been removed from user path.")
